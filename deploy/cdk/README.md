70fc42cc74d4279db235b0254ce35a258c7869bd
Yoshiharu Hirose <yh1224@gmail.com>
2024-01-02 09:54:10 +0900

    implement cdk

--- deploy/cdk/README.md
+++ deploy/cdk/README.md
@@ -1,14 +1,22 @@
-# Welcome to your CDK TypeScript project
+# AWS AutoStop
 
-This is a blank project for CDK development with TypeScript.
+## Prerequisite
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+- [AWS CDK](https://aws.amazon.com/cdk/)
 
-## Useful commands
+## How to deploy
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+1. Create config.yaml and configure
+
+2. Prepare
+
+   ```shell
+   npm install
+   cdk bootstrap
+   ```
+
+3. Deploy stack
+
+   ```shell
+   cdk deploy
+   ```
--- deploy/cdk/lib/aws-autostop-stack.ts
+++ deploy/cdk/lib/aws-autostop-stack.ts
@@ -1,16 +1,86 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as events from "aws-cdk-lib/aws-events";
+import * as events_targets from "aws-cdk-lib/aws-events-targets";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambda_python from "@aws-cdk/aws-lambda-python-alpha";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import {Construct} from "constructs";
+import {Config} from "./config";
 
-export class AwsAutostopStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+type AwsAutoStopStackProps = cdk.StackProps & {
+    readonly config: Config;
+}
 
-    // The code that defines your stack goes here
+export class AwsAutoStopStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: AwsAutoStopStackProps) {
+        super(scope, id, props);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsAutostopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const {config} = props;
+        const timezone = config.timezone ?? "UTC";
+        const slackWebhookUrl = config.slackWebhookUrl ?? "";
+
+        const autoStopTopic = new sns.Topic(this, "AutoStopTopic");
+
+        const autoStopRole = new iam.Role(this, "AutoStopRole", {
+            assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
+            ],
+            inlinePolicies: {
+                "AutoStopPolicy": new iam.PolicyDocument({
+                    statements: [
+                        new iam.PolicyStatement({
+                            actions: [
+                                "autoscaling:DescribeAutoScalingGroups",
+                                "autoscaling:UpdateAutoScalingGroup",
+                                "ec2:DescribeInstances",
+                                "ec2:DescribeInstanceStatus",
+                                "ec2:StartInstances",
+                                "ec2:StopInstances",
+                                "elasticbeanstalk:DescribeEnvironments",
+                                "elasticbeanstalk:DescribeEnvironmentResources",
+                                "elasticbeanstalk:ListTagsForResource",
+                                "elasticbeanstalk:UpdateEnvironment",
+                                "rds:DescribeDBClusters",
+                                "rds:DescribeDBInstances",
+                                "rds:ListTagsForResource",
+                                "rds:StartDBCluster",
+                                "rds:StartDBInstance",
+                                "rds:StopDBCluster",
+                                "rds:StopDBInstance",
+                            ],
+                            effect: iam.Effect.ALLOW,
+                            resources: ["*"],
+                        }),
+                        new iam.PolicyStatement({
+                            actions: ["sns:Publish"],
+                            effect: iam.Effect.ALLOW,
+                            resources: [autoStopTopic.topicArn],
+                        }),
+                    ],
+                }),
+            },
+        });
+
+        const autoStopFunction = new lambda_python.PythonFunction(this, "AutoStopFunction", {
+            architecture: lambda.Architecture.ARM_64,
+            entry: "../../src/lambdas/AutoStopFunction",
+            environment: {
+                TIMEZONE: timezone,
+                NOTIFY_TOPIC_ARN: autoStopTopic.topicArn,
+                SLACK_WEBHOOK_URL: slackWebhookUrl,
+            },
+            handler: "lambda_handler",
+            logRetention: logs.RetentionDays.ONE_WEEK,
+            runtime: lambda.Runtime.PYTHON_3_11,
+            role: autoStopRole,
+            timeout: cdk.Duration.seconds(10),
+        });
+        new events.Rule(this, "AutoStopRule", {
+            schedule: events.Schedule.cron({minute: "0"}), // every hour
+            targets: [new events_targets.LambdaFunction(autoStopFunction)],
+        });
+    }
 }
